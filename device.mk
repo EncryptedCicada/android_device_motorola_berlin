0d09d4e806cf4f8cb2c93ab226452d083a5936b1
EncryptedCicada <dev.varnit@gmail.com>
2025-04-07 13:49:22 +0530

    Go back to building spkr prot lib

--- device.mk
+++ device.mk
@@ -27,6 +27,10 @@ PRODUCT_PACKAGES += \
     SystemUIResBerlin \
     WifiResBerlin
 
+# audio
+PRODUCT_PACKAGES += \
+		libcirrusspkrprot
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sku_yupik/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/sku_yupik/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/mixer_paths.xml \
